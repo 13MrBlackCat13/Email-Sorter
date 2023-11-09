9fbc720bfc3563d5519513f86ac0adfb33f2af52
Mykhailo Moskalets <69348201+13MrBlackCat13@users.noreply.github.com>
2023-11-09 15:28:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
 import os
 import requests
+import logging
+import shutil
+from dotenv import load_dotenv
 
-# Параметры для API
-API_URL = "http://localhost:5000/api/emailcategory/eml"
-SUPER_TOKEN = "devtest"
-TOKEN_GROUP = "group1"  # Выберите группу токена, которую хотите использовать
+# Загрузка переменных окружения
+load_dotenv()
+API_URL = os.getenv("API_URL", "http://localhost:5000/api/emailcategory/eml")
+SUPER_TOKEN = os.getenv("SUPER_TOKEN", "devtest")
+TOKEN_GROUP = os.getenv("TOKEN_GROUP", "group1")
+UNSORTED_DIR = os.getenv("UNSORTED_DIR", "Unsorted")
+PROBABILITY_THRESHOLD = float(os.getenv("PROBABILITY_THRESHOLD", "0.6"))
 
-# Путь к папке Unsorted
-unsorted_dir = "Unsorted"
+# Настройка логгера
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-probability_threshold = 0.6
+def read_eml_file(path):
+    try:
+        with open(path, "rb") as f:
+            return f.read()
+    except IOError as e:
+        logging.error(f"Error reading file {path}: {e}")
+        return None
 
-# Пройдемся по всем .eml файлам в папке Unsorted
-for root, dirs, files in os.walk(unsorted_dir):
-    for file in files:
-        if file.endswith(".eml"):
-            eml_path = os.path.join(root, file)
+def process_file(file_path, file_name):
+    eml_content = read_eml_file(file_path)
+    if eml_content is None:
+        return
 
-            # Чтение содержимого .eml файла
-            with open(eml_path, "rb") as f:
-                eml_content = f.read()
+    data = {"model_name": "all", "text_only": "true"}
 
-            # Параметры для запроса к API
-            data = {
-                "model_name": "all",  # Используем все модели для анализа
-                "text_only": "true"
-            }
+    try:
+        response = requests.post(API_URL, files={"file": (file_name, eml_content, "message/rfc822")}, params=data)
+        if response.status_code == 200:
+            handle_response(response.json(), file_path, file_name)
+        else:
+            logging.error(f"Error processing {file_name}: {response.text}")
+    except requests.RequestException as e:
+        logging.error(f"Request failed: {e}")
 
-            # Отправляем запрос к API
-            response = requests.post(API_URL, files={"file": (file, eml_content, "message/rfc822")}, params=data)
+def handle_response(response_data, file_path, file_name):
+    avg_probabilities = response_data[-1]
+    category_probabilities = {k: v for k, v in avg_probabilities.items() if k != 'Model'}
 
-            if response.status_code == 200:
-                results = response.json()  # Получаем все результаты из списка
-                avg_probabilities = results[-1]  # Берем последний элемент, который содержит средние вероятности
-                category_probabilities = {k: v for k, v in avg_probabilities.items() if k != 'Model'}
-                
-                # Пройдемся по категориям и проверим, превышает ли средняя вероятность порог
-                for category, probability in category_probabilities.items():
-                    if float(probability) >= probability_threshold:
-                        sorted_folder = os.path.join("Sorted", category)
+    for category, probability in category_probabilities.items():
+        if float(probability) >= PROBABILITY_THRESHOLD:
+            move_file_to_category(file_path, file_name, category)
+            break
 
-                        # Если папка для категории не существует, создаем её
-                        if not os.path.exists(sorted_folder):
-                            os.makedirs(sorted_folder)
+def move_file_to_category(file_path, file_name, category):
+    sorted_folder = os.path.join("Sorted", category)
+    if not os.path.exists(sorted_folder):
+        os.makedirs(sorted_folder)
 
-                        # Перемещаем .eml файл в соответствующую папку
-                        new_path = os.path.join(sorted_folder, file)
-                        os.rename(eml_path, new_path)
-                        print(f"Moved {file} to {sorted_folder}")
-                        break  # Перемещаем файл только в первую папку, которая подходит
+    new_path = os.path.join(sorted_folder, file_name)
+    shutil.move(file_path, new_path)
+    logging.info(f"Moved {file_name} to {sorted_folder}")
 
-            else:
-                print(f"Error processing {file}: {response.text}")
+def main():
+    for root, dirs, files in os.walk(UNSORTED_DIR):
+        for file in files:
+            if file.endswith(".eml"):
+                process_file(os.path.join(root, file), file)
+
+if __name__ == "__main__":
+    main()
