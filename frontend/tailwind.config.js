601d814d6b2bab57c5b8468690490e49eb0d33f9
Mykhailo Moskalets <tishka3112@gmail.com>
2024-09-04 19:23:41 +0200

    забыл...

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+      textColor: ['active'],
+    },
+  },
+  plugins: [],
+}
